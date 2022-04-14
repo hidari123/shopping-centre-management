619db7ea811b7ee745d7434a7797067e202f0511
lijiaying <1640106564@qq.com>
2022-04-14 12:46:39 +0800

    vuex持久化&&axios封装

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
+
+// 模块
+import user from './modules/user'
+import cart from './modules/cart'
+import category from './modules/category'
 
 // vue2 => new Vue.store({})
 // vue3 => createStore({})
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    user,
+    cart,
+    category
+  },
+  // 配置插件
+  plugins: [createPersistedstate({
+    // 本地存储名字
+    key: 'shopping-centre-management-store',
+    // 指定需要存储的模块
+    paths: ['user', 'cart']
+  })]
 })
